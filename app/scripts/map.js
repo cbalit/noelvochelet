c9a6aa4c74303aca8af8984ba4fb80d464f41f79
Cyril Balit <cbalit@gmail.com>
2015-07-05 13:11:17 +0200

    add some pages

---
+++ app/scripts/map.js
@@ -0,0 +1,20 @@
+function init_map() {
+    var var_location = new google.maps.LatLng(48.7587539,0.80915);
+    var var_mapoptions = {
+        center: var_location,
+        zoom: 14
+    };
+
+    var var_marker = new google.maps.Marker({
+        position: var_location,
+        map: var_map,
+        title:"Noel Vochelet"});
+
+    var var_map = new google.maps.Map(document.getElementById("map-container"),
+        var_mapoptions);
+
+    var_marker.setMap(var_map);
+
+}
+
+google.maps.event.addDomListener(window, 'load', init_map);
